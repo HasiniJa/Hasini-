45598c6d63b61327c8d80e1bf57505252c85e5da
HasiniJa <hasinijayasekara2012@gmail.com>
2024-05-13 00:10:35 -0400

    commiting Lab01.Bpy

---
+++ Lab-01/Lab01-B.py
@@ -0,0 +1,15 @@
+'''Welcome to PRG600
+Enter your name :Eric
+Enter your student ID: 123456789
+Enter your Age:41
+Enter your favourite food: Pizza
+Eric is 41 years Old and loves to eat Pizza
+Good Luck with the Course
+'''
+print("Welcome to PRG600")
+user_name=input("Enter your name")
+user_id=input("Enter your student id")
+age=input("Enter your Age")
+fav_food=input("Enter your Favourite food")
+print(""+user_name+"is"+age+"years old and loves to eat"+fav_food+"")
+print("Good Luck with the Course")
