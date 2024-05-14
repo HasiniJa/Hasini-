9deece1a1496b44266eab03f11e9729d66e0e1cb
HasiniJa <hasinijayasekara2012@gmail.com>
2024-05-14 09:23:13 -0400

    code changes

--- Lab-01/Lab1a.py
+++ Lab-01/Lab1a.py
@@ -3,6 +3,8 @@ Name: Hasini
 Student ID : 135513235
 Description: Lab 01 - Question 01
 '''
+#this is the lab1 a part 
+
 print("Hello world")
 user_name =input("input your name")
 print("Hello"+user_name +"!")
--- Lab-01/Lab1b.py
+++ Lab-01/Lab1b.py
@@ -1,15 +1,17 @@
-'''Welcome to PRG600
+'''Welcome to PRG600                             
 Enter your name :Eric
 Enter your student ID: 123456789
-Enter your Age:41
+Enter your age:41
 Enter your favourite food: Pizza
 Eric is 41 years Old and loves to eat Pizza
 Good Luck with the Course
 '''
-print("Welcome to PRG600")
-user_name=input("Enter your name")
-user_id=input("Enter your student id")
-age=input("Enter your Age")
-fav_food=input("Enter your Favourite food")
-print(user_name+"is"+age+"years old and loves to eat"+fav_food+"!")
-print("Good Luck with the Course")
+#this is the lab1 b part- discusses on the variables and the statemnts
+
+print("Welcome to PRG600!")
+user_name=input("Enter your name:") #  ask to input the user name
+user_id=input("Enter your student ID:") #ask to input the student ID
+age=input("Enter your age:")#ask to input the age
+fav_food=input("Enter your favorite food:") #ask to input favorite food
+print(user_name+"is"+age+"years old and loves to eat"+fav_food+"!") #ask to print all the input values in one sentence
+print("Good Luck with the course!")
