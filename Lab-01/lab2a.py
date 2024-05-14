e7c398ddcbeaabf1925d50b57d9cd203958a400d
HasiniJa <hasinijayasekara2012@gmail.com>
2024-05-14 10:57:45 -0400

    commiting lab2a.py

---
+++ Lab-01/lab2a.py
@@ -0,0 +1,16 @@
+# Name : Hasini Jayasekara
+# Student ID : 165513235
+
+#asking to input the numbers
+user_num1 = input("Enter the first number ")
+user_num2 = input("Enter the second number")
+
+#converting them to Int(turning in to numeric values)
+user_int1 = int(user_num1)
+user_int2 = int(user_num2)
+
+#adding the two numbers
+result = user_int1+user_int2
+
+#printing the result
+print(" The result of "+user_int1+"plus"+user_int2+" is :"+result)
