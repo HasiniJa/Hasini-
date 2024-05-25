d7399da299cf2ad4e2c1011acfb227b9e73b1841
HasiniJa <hasinijayasekara2012@gmail.com>
2024-05-25 16:31:05 -0400

    commiting lab3b.py

---
+++ Lab-02/lab3b.py
@@ -0,0 +1,25 @@
+'''Student Name : Hasini Jayasekara
+   Student ID : 165513235
+   Lab-02 : print numbers 0 to 10
+'''
+'''
+Sample Output 
+0 
+1 
+2 
+3 
+4 
+5 
+6 
+7 
+8 
+9 
+10 
+finished 
+'''
+num=0
+while num <=10:
+    print(num)
+    num+=1
+print("finished")
+
